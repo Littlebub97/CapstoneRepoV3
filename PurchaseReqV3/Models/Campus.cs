0406d55e37ee29e01b43a312922e70ff4f6174a0
Alexander Sidwell <rlilwoman77@aol.com>
2019-04-12 12:48:38 -0400

    I'm not liking this

---
+++ PurchaseReqV3/Models/Campus.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PurchaseReqV3.Models
+{
+    [Table("Campus", Schema = "PurchaseReq")]
+    public class Campus : Base
+    { 
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        //Consider changing the length
+        //Consider making a model
+        [StringLength(50)]
+        public string Address { get; set; }
+        //oof
+        //Double oof
+        //Triple oof
+        //So many oofs
+
+    }
+}
