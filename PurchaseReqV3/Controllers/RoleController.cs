c038aacb85791e0b818f10202fab82edf89c18e6
Alexander Sidwell <rlilwoman77@aol.com>
2019-04-18 13:41:20 -0400

    Getting roles together

---
+++ PurchaseReqV3/Controllers/RoleController.cs
@@ -0,0 +1,33 @@
+﻿using PurchaseReqV3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PurchaseReqV3.Controllers
+{
+    public class RoleController : Controller
+    {
+        // GET: Role
+        public ActionResult Index()
+        {
+            if(User.Identity.IsAuthenticated)
+            {
+
+
+                if (!isAdminUser())
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }   
+            else   
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            var Roles = context.Roles.ToList();
+            return View(Roles);
+        }
+    }
+}
