ec643199d58c976e3ee92fa16226db3b7931d126
Alexander Sidwell <rlilwoman77@aol.com>
2019-04-18 13:46:27 -0400

    Fixed the role stuff again

--- PurchaseReqV3/Controllers/RoleController.cs
+++ PurchaseReqV3/Controllers/RoleController.cs
@@ -1,4 +1,6 @@
-﻿using PurchaseReqV3.Models;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using PurchaseReqV3.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +11,7 @@ namespace PurchaseReqV3.Controllers
 {
     public class RoleController : Controller
     {
+        ApplicationDbContext context = new ApplicationDbContext();
         // GET: Role
         public ActionResult Index()
         {
@@ -29,5 +32,25 @@ namespace PurchaseReqV3.Controllers
             var Roles = context.Roles.ToList();
             return View(Roles);
         }
+        public Boolean isAdminUser()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var user = User.Identity;
+                ApplicationDbContext context = new ApplicationDbContext();
+                var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+                var s = UserManager.GetRoles(user.GetUserId());
+                if (s[0].ToString() == "Admin")
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
     }
+
 }
